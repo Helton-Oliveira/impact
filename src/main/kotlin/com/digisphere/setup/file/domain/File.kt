9dbe7c29a44ac66ffda690d10b37af382bf06b59
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-10-30 20:47:25 -0300

    refactor: Ajustando entidade file para ficar mais semantica

--- src/main/kotlin/com/digisphere/setup/file/domain/File.kt
+++ src/main/kotlin/com/digisphere/setup/file/domain/File.kt
@@ -3,10 +3,7 @@ package com.digisphere.setup.file.domain
 import com.digisphere.setup.config.root.BaseEntity
 import com.digisphere.setup.file.enum.FileType
 import com.digisphere.setup.user.domain.User
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "adm_files")
@@ -14,6 +11,8 @@ class File(
     val name: String,
     val path: String?,
     val base64: String?,
+
+    @Enumerated(EnumType.STRING)
     val type: FileType,
 ) : BaseEntity() {
 
