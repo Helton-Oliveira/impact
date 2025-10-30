fd55b34ac938209a56de3e10253b3900f8483395
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-10-30 20:48:44 -0300

    refactor: Ajustando metodo para buscar por email

--- src/main/kotlin/com/digisphere/setup/user/repository/UserRepository.kt
+++ src/main/kotlin/com/digisphere/setup/user/repository/UserRepository.kt
@@ -4,7 +4,9 @@ import com.digisphere.setup.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository : JpaRepository<User, Long> {
 
     @Query(
