60cf69bb282f6ff61bfef4ffd0fbe0785acb8a58
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-11-03 12:33:44 -0300

    feat: Adicionando metodos para buscar um usuario, buscar a lista e realizar a  exclusao logica

--- src/main/kotlin/com/digisphere/setup/user/resource/UserResource.kt
+++ src/main/kotlin/com/digisphere/setup/user/resource/UserResource.kt
@@ -1,16 +1,20 @@
 package com.digisphere.setup.user.resource
 
+import com.digisphere.setup.user.domain.UserAssociations
 import com.digisphere.setup.user.dto.UserInput
 import com.digisphere.setup.user.dto.UserOutput
 import com.digisphere.setup.user.service.UserService
 import jakarta.validation.ConstraintViolationException
 import jakarta.validation.Valid
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
@@ -20,8 +24,8 @@ class UserResource(
 ) {
 
     @PostMapping
-    fun create(@RequestBody @Valid input: UserInput, uriBuilder: UriComponentsBuilder): ResponseEntity<UserOutput> {
-        return userService.create(input)
+    fun create(@RequestBody @Valid input: UserInput, uriBuilder: UriComponentsBuilder): ResponseEntity<UserOutput> =
+        userService.create(input)
             .fold(
                 onFailure = { error ->
                     val status = when (error) {
@@ -35,7 +39,56 @@ class UserResource(
                         .body(output)
                 }
             )
-    }
 
+    @GetMapping("/{id}")
+    fun getOne(@PathVariable id: Long): ResponseEntity<UserOutput> =
+        userService.getOne(id, setOf(UserAssociations.FILES))
+            .fold(
+                onFailure = { err ->
+                    val status = when (err) {
+                        is HttpClientErrorException.Unauthorized -> HttpStatus.UNAUTHORIZED
+                        is IllegalArgumentException -> HttpStatus.CONFLICT
+                        else -> HttpStatus.INTERNAL_SERVER_ERROR
+                    }
+                    ResponseEntity.status(status).body(null)
+                },
+                onSuccess = { ResponseEntity.ok(it) }
+            )
+
+    @GetMapping
+    fun getAll(
+        @PageableDefault(size = 10, sort = ["createdAt"], direction = Sort.Direction.DESC) pagination: Pageable
+    ): ResponseEntity<Page<UserOutput>> =
+        userService.getAll(pagination)
+            .fold(
+                onFailure = { err ->
+                    val status = when (err) {
+                        is HttpClientErrorException.Unauthorized -> HttpStatus.UNAUTHORIZED
+                        is IllegalArgumentException -> HttpStatus.CONFLICT
+                        else -> HttpStatus.INTERNAL_SERVER_ERROR
+                    }
+
+                    ResponseEntity.status(status).body(null)
+                },
+                onSuccess = { ResponseEntity.ok(it) }
+            )
+
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN, SYSTEM_ADMIN')")
+    fun disabled(@PathVariable id: Long): ResponseEntity<UserOutput> =
+        userService.disableById(id)
+            .fold(
+                onFailure = { err ->
+                    val status = when (err) {
+                        is HttpClientErrorException.Unauthorized -> HttpStatus.UNAUTHORIZED
+                        is IllegalArgumentException -> HttpStatus.CONFLICT
+                        else -> HttpStatus.INTERNAL_SERVER_ERROR
+                    }
+
+                    ResponseEntity.status(status).body(null)
+                },
+                onSuccess = { ResponseEntity.ok(it) }
+            )
 
 }
