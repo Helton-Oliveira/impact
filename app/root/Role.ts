b85105b1dbab151dcbec978480eff0637a40d8dc
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-11-07 18:01:17 -0300

    deletando arquivos desnecessários e adicionando novos

---
+++ app/root/Role.ts
@@ -0,0 +1,5 @@
+export enum Role {
+    SYSTEM_ADMIN,
+    ADMIN,
+    USER,
+}
---
+++ app/user/model/user.entity.ts
@@ -0,0 +1,16 @@
+import {BaseEntity} from "@/app/root/BaseEntity";
+import {Role} from "@/app/root/Role";
+
+export default class User extends BaseEntity {
+    public firstName?: string;
+    public lastName?: string;
+    public cpf?: string;
+    public email?: string;
+    public phoneNumber?: string;
+    public role?: Role;
+    public password?: string;
+    public resetKey?: string;
+    public resetKeyCreatedAt?: number;
+    public files?: File[];
+
+}
