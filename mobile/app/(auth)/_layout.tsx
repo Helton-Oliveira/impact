7bf6895a64d8a89b18cc2282d49ce36a8e9c7644
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-11-13 18:48:41 -0300

    feat: mapeando novas rotas e estilizando o header

--- mobile/app/(auth)/_layout.tsx
+++ mobile/app/(auth)/_layout.tsx
@@ -3,12 +3,27 @@ import {Stack} from "expo-router";
 export default function AuthLayout() {
     return (
         <Stack
-            initialRouteName="login"
+            initialRouteName="login/index"
             screenOptions={{
-                headerShown: false,
+                headerStyle: {
+                    backgroundColor: "#12211F",
+                },
+                headerTitleAlign: "center",
+                headerTintColor: "#FFF",
+                headerTitleStyle: {
+                    fontWeight: "bold",
+                    color: "#FFF",
+                    fontSize: 25
+                },
+                headerBackTitle: ""
             }}
         >
-            <Stack.Screen name="login" options={{headerShown: false}}/>
+            <Stack.Screen name="login/index" options={{headerShown: false}}/>
+            <Stack.Screen name="createAccount/index"
+                          options={{
+                              headerTitle: "Login",
+                          }}
+            />
         </Stack>
     );
 }
