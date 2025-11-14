d99dd7bc380c47dcffa54de56329a25fda7869b8
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-11-14 07:54:28 -0300

    chore: Adicionando tanstack react query e react devTools ao projeto

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,9 +1,19 @@
 import {Stack} from "expo-router";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name="(auth)" options={{headerShown: false}}/>
-        </Stack>
+        <QueryClientProvider client={queryClient}>
+            <Stack>
+                <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+            </Stack>
+
+            {__DEV__ && (
+                <ReactQueryDevtools initialIsOpen={false}/>
+            )}
+        </QueryClientProvider>
     );
 }
