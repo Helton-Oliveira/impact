953cb74dd3d50f32206c06eb4c31af20119fc2a0
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-11-21 23:13:21 -0300

    feat: Adicionando funcao para visualizar todas as campanhas

--- mobile/app/(tabs)/home/_useHome.tsx
+++ mobile/app/(tabs)/home/_useHome.tsx
@@ -1,11 +1,16 @@
 import _useCampaignQuery from "@/queries/campaign.query";
 import _useLoginQuery from "@/app/(auth)/login/_login.query";
+import {router} from "expo-router";
 
 export const _useHome = () => {
     const {getCurrentUser} = _useLoginQuery()
     const {getAll} = _useCampaignQuery();
 
     const {data: user} = getCurrentUser();
+
+    function goCampaignList() {
+        router.replace("/campaignList");
+    }
 
     const imageUri = user?.files?.[0]?.path || `data:image/jpeg;base64,${user?.files?.[0]?.base64}`;
 
@@ -15,6 +20,7 @@ export const _useHome = () => {
         isErrorCampaigns: getAll.isError,
         isSuccessCampaigns: getAll.isSuccess,
         user,
-        imageUri
+        imageUri,
+        goCampaignList
     }
 }
