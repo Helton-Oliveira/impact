41aa1c4129d50098a2ce3b924cc1a29719995365
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-11-21 23:12:30 -0300

    feat: Atualizando entidade no front de campanhas com os campos novos

--- mobile/src/campaign/campaign.model.tsx
+++ mobile/src/campaign/campaign.model.tsx
@@ -2,14 +2,14 @@ import {BaseEntity} from "@/src/root/BaseEntity";
 import User from "@/src/user/user.model";
 import AppFile from "@/src/file/file.mode";
 import {Status} from "@/src/enumerations/status";
-import {TypeOfDonation} from "@/src/enumerations/typeOfDonation";
 
 export default class Campaign extends BaseEntity {
     public name?: string;
     public purpose?: string;
     public expirationTime?: Date;
     public status?: Status;
-    public typeOfDonations?: TypeOfDonation[];
+    public allowMoneyDonation?: boolean;
+    public allowItemDonation?: boolean;
     public file?: AppFile;
     public user?: User;
 }
