f2420cbd62ad1265ad60916749ecf5e27cdf3551
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-11-21 23:12:54 -0300

    feat: Implementando query para obter access token pelo refresh token

--- mobile/src/auth/auth.service.ts
+++ mobile/src/auth/auth.service.ts
@@ -1,13 +1,21 @@
 import {LoginRequest, LoginResponse} from "@/src/auth/dto/login.dto";
 import api from "@/src/root/api";
 
-type ResponseType = LoginResponse;
+type EntityResponseType = LoginResponse;
 
 export default class AuthService {
 
-    async login(loginRequest: LoginRequest): Promise<ResponseType> {
+    async login(loginRequest: LoginRequest): Promise<EntityResponseType> {
         const response = await api.post(`/auth/login`, loginRequest);
         return await response.data;
+    }
+
+    async getNewAccessToken(refreshToken: string): Promise<EntityResponseType> {
+        const params = {
+            refreshToken: refreshToken
+        }
+        return api.post(`/auth/refresh`, params)
+            .then(res => res.data)
     }
 
 }
